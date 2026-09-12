81ac301fd04fb45cac24f29492f5e4b3fb3e76cf
darlanbelfortofcl <darlanbelfortofcl@gmail.com>
2026-09-12 19:31:18 -0300

    Nova Atualizacao

--- exercicio003.py
+++ exercicio003.py
@@ -27,4 +27,4 @@ while True:
         print("Resultado: ",num1*num2)
     elif operador == 4:
         print("Resultado: ",num1/num2)
-print("Obrigado por usar nosso sistema ✅")
+    print("Obrigado por usar nosso sistema ✅")
